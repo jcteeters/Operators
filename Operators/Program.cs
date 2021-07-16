d118cb91558da98e37158b54a943de1ea5b72a69
Jacob Teeters <jcteeters7@gmail.com>
2021-07-16 09:13:04 -0700

    Add project files.

---
+++ Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4);
+            Console.Write($"{a}/{quotient} is {b} remainder {remainder}");
+        }
+    }
+}
