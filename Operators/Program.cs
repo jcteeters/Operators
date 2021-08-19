584287ef2c1d42a2114bfbc1851ca94c73f631e5
Jacob Teeters <jcteeters7@gmail.com>
2021-08-19 14:12:00 -0500

    commit

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -10,8 +10,32 @@ namespace Operators
             int b = 4;
             int quotient = a / b;
             int remainder = a % b;
-            if (a == 17 && b == 4);
-            Console.Write($"{a}/{quotient} is {b} remainder {remainder}");
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine("First Problem:");
+                Console.WriteLine("17 / 4 and the calculated remainder");
+                Console.WriteLine($"{a}/{quotient} is {b} remainder {remainder}  ");
+                Console.WriteLine("------------");
+            }
+
+            Console.WriteLine("Second Problem:");
+            Console.WriteLine("Let me calculate the area of your circle");
+                Console.WriteLine("But first enter the radius of yor circle ");
+                var userInput = Console.ReadLine();
+
+                var radius = double.Parse(userInput);
+
+                AreaOfCircle(radius);
+                                                       
+            static double AreaOfCircle(double radius)
+            {
+                var area = Math.PI * Math.Pow(radius, 2);
+
+                Console.WriteLine($"The area of your circle is: {area}");
+
+                return area;
+            }
         }
+       
     }
 }
